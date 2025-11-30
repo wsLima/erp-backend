a15b64cccb4935d034de047e1006c0a720dfc2ba
Kennedy Lima <kennedy.wslima@gmail.com>
2025-11-29 21:02:16 -0400

    first commit

---
+++ src/main/java/br/com/wsystechnologies/erp/ErpBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.wsystechnologies.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wsystechnologies/erp/ErpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsystechnologies.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
